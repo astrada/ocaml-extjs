a941d346377b284f7e2f207c9ebeefa2e582d110
Alessandro Strada <alessandro.strada@gmail.com>
2012-08-16 22:27:01 +0200

    Refactor controller Users.ml (account manager)

--- examples/account_manager/app/controller/Users.ml
+++ examples/account_manager/app/controller/Users.ml
@@ -7,59 +7,54 @@ class type t = object
 end
 
 let () =
-  let userlist_listeners =
-    ExtUtils.new_config
-      (fun (obj : Ext_AbstractComponent.events Js.t) ->
-         obj##itemdblclick <- Js.wrap_meth_callback
-                                (fun this grid record
-                                       item index e eOpts ->
-                                   this##editUser(grid, record))
-      ) in
-  let useredit_listeners =
-    ExtUtils.new_config
-      (fun (obj : Ext_AbstractComponent.events Js.t) ->
-         obj##click <- Js.wrap_meth_callback
-                         (fun this button e eOpts ->
-                            this##updateUser(button))
-      ) in
-  let controller_conf =
-    ExtUtils.new_config
-      (fun (obj : t Js.t) ->
-         obj##extend <- Js.string "Ext.app.Controller";
-         obj##stores <- Js.array [|Js.string "Users"|];
-         obj##models <- Js.array [|Js.string "User"|];
-         obj##views <- Js.array [|Js.string "user.List";
-                                  Js.string "user.Edit"|];
-         obj##init <- Js.wrap_meth_callback
+  let userlist_listeners : Ext_AbstractComponent.events Js.t = {| |} in
+  userlist_listeners##itemdblclick <- Js.wrap_meth_callback
+                                        (fun this grid record
+                                               item index e eOpts ->
+                                           this##editUser(grid, record));
+
+  let useredit_listeners : Ext_AbstractComponent.events Js.t = {| |} in
+  useredit_listeners##click <- Js.wrap_meth_callback
+                                 (fun this button e eOpts ->
+                                    this##updateUser(button));
+
+  let controller : t Js.t =
+    {| extend = Js.string "Ext.app.Controller";
+       stores = Js.array [|Js.string "Users"|];
+       models = Js.array [|Js.string "User"|];
+       views = Js.array [|Js.string "user.List";
+                          Js.string "user.Edit"|];
+     |}
+  in
+
+  controller##init <- Js.wrap_meth_callback
                         (fun this _ ->
                            let selectors_listeners =
-                             jsnew (Js.Unsafe.variable "Object") () in
-                             ExtUtils.assoc_set
-                               selectors_listeners
-                               (Js.string "viewport > userlist")
-                               userlist_listeners;
-                             ExtUtils.assoc_set
-                               selectors_listeners
-                               (Js.string "useredit button[action=save]")
-                               useredit_listeners;
-                             this##control(selectors_listeners));
-         obj##editUser <- Js.wrap_callback
+                             Js.Unsafe.obj
+                               [| ("viewport > userlist",
+                                   Js.Unsafe.inject userlist_listeners);
+                                  ("useredit button[action=save]",
+                                   Js.Unsafe.inject useredit_listeners);
+                               |]
+                           in
+                           this##control(selectors_listeners));
+  controller##editUser <- Js.wrap_callback
                             (fun grid record ->
                                let view = Ext.widget ~name:"useredit" in
                                  view##down(Js.string "form")##loadRecord(record));
-         obj##updateUser <- Js.wrap_meth_callback
+  controller##updateUser <- Js.wrap_meth_callback
                               (fun this button ->
                                  let win = button##up(Js.string "window") in
                                  let form = win##down(Js.string "form") in
                                  let record = form##getRecord () in
                                  let values = form##getValues () in
 
-                                   record##set (values);
-                                   win##close ();
-                                   this##getUsersStore()##sync());
-      ) in
+                                 record##set (values);
+                                 win##close ();
+                                 this##getUsersStore()##sync());
+
   Ext.define
     ~className:"AM.controller.Users"
-    ~data:controller_conf
+    ~data:controller
     ()
 
