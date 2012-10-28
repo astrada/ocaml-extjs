9f2d93ad5fdac1d1a0bf82edb2fb7836b9b4edf5
Alessandro Strada <alessandro.strada@gmail.com>
2012-10-29 00:20:48 +0100

    Removed dependency on js_of_ocaml.syntax

--- lib/extUtils.mli
+++ lib/extUtils.mli
@@ -9,9 +9,6 @@ external assoc_set : 'a -> 'b -> 'c -> unit = "caml_js_assoc_set"
 external call_parent : 'a -> string -> unit = "caml_js_call_parent"
 (** [call_parent o n] calls method [n] of the superclass of object [o] *)
 
-val new_config : ('a Js.t -> unit) -> 'a Js.t
-(** [new_config s] creates a new object setting it up using function [s] *)
-
 val log : string -> unit
 (** [log m] logs message [m] to Javascript console *)
 
