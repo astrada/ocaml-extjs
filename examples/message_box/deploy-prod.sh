f23f1ebe63d83a9143bdcea44958b4bf97b69189
Alessandro Strada <alessandro.strada@gmail.com>
2013-03-26 22:41:04 +0100

    Update message box example to ExtJS 4.2

--- examples/message_box/deploy-prod.sh
+++ examples/message_box/deploy-prod.sh
@@ -8,24 +8,33 @@ SITEROOT="$WWWROOT/$SITENAME"
 set -e
 
 # build
+create_dir js
 cd ../..
 ocaml setup.ml -build
-cp "_build/examples/$SITENAME/msg_box.js" "examples/$SITENAME/app.js"
+cp "_build/examples/$SITENAME/msg_box.js" "examples/$SITENAME/js"
 cd -
+
+# append dependencies
+echo "//@require Ext.window.MessageBox//@require Ext.tip.*" >> js/msg_box.js
 
 # minify
 link_extjs extjs
-$SENCHA create jsb -a index.html -p app.jsb3
-$SENCHA build -p app.jsb3 -d .
+$SENCHA compile \
+  -classpath=extjs/src,js \
+  page \
+  -compress \
+  -in index-prod.html \
+  -out index-out.html
 
 # deploy
 create_dir "$SITEROOT"
 link_extjs "$SITEROOT/extjs"
-cp index-prod.html "$SITEROOT/index.html"
+cp index-out.html "$SITEROOT/index.html"
 create_dir "$SITEROOT/images"
 cp images/* "$SITEROOT/images"
-cp app-all.js $SITEROOT
+cp all-classes.js $SITEROOT
 
 # clean up
-rm extjs app.js app.jsb3 all-classes.js app-all.js
+rm extjs js/msg_box.js all-classes.js index-out.html
+rmdir js
 
