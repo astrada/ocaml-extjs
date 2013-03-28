eda4c4a3ff8aa349f64c7a8c823eac347ac32ab9
Alessandro Strada <alessandro.strada@gmail.com>
2013-03-28 22:48:23 +0100

    Update window layout example to ExtJS 4.2

--- examples/window/layout/deploy-prod.sh
+++ examples/window/layout/deploy-prod.sh
@@ -8,22 +8,31 @@ SITEROOT="$WWWROOT/$SITENAME"
 set -e
 
 # build
+create_dir js
 cd ../../..
 ocaml setup.ml -build
-cp "_build/examples/$SITENAME/layout.js" "examples/$SITENAME/app.js"
+cp "_build/examples/$SITENAME/layout.js" "examples/$SITENAME/js"
 cd -
+
+# append dependencies
+echo "//@require Ext.tab.*//@require Ext.window.*//@require Ext.tip.*//@require Ext.layout.container.Border" >> js/layout.js
 
 # minify
 link_extjs extjs
-$SENCHA create jsb -a index.html -p app.jsb3
-$SENCHA build -p app.jsb3 -d .
+$SENCHA compile \
+  -classpath=extjs/src,js \
+  page \
+  -compress \
+  -in index-prod.html \
+  -out index-out.html
 
 # deploy
 create_dir "$SITEROOT"
 link_extjs "$SITEROOT/extjs"
-cp index-prod.html "$SITEROOT/index.html"
-cp app-all.js $SITEROOT
+cp index-out.html "$SITEROOT/index.html"
+cp all-classes.js $SITEROOT
 
 # clean up
-rm extjs app.js app.jsb3 all-classes.js app-all.js
+rm extjs js/layout.js all-classes.js index-out.html
+rmdir js
 
